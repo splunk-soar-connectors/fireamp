7ab43f43881df3740607af581d23af8044baf1ae
Atif Mahadik <atif@phantom.us>
2018-01-24 18:59:02 -0800

    PAPP-3780 3.5 copyright compatability changes

--- fireamp_consts.py
+++ fireamp_consts.py
@@ -1,4 +1,16 @@
-AMP_JSON_API_CLIENT_ID = "api_client_id"
+# --
+# File: fireamp_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --AMP_JSON_API_CLIENT_ID = "api_client_id"
 AMP_JSON_API_KEY = "api_key"
 AMP_JSON_HASH = "hash"
 AMP_JSON_IP = "ip"
